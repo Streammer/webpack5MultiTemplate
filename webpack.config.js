d05ea76a977494d02aa08c540c16abe19629afb2
Dmitrij <lukdmitrij@yandex.ru>
2023-03-28 22:12:04 +0300

    patch had been changed from join to resolve

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'template.html'),
-      // template: path.join(__dirname, 'src', 'template.pug'),
+      // template: path.resolve(__dirname, 'src', 'template.pug'),
       filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
